150a0a0222ea5ee4f8e78c26d74d8884c7c3e0de
cr7st74n <cris-082011@hotmail.com>
2023-05-16 21:55:57 -0400

    adding stiles and firts elements to our project

--- JS/TicTacToe.js
+++ JS/TicTacToe.js
@@ -1,6 +1,29 @@
 
-for(let i = 0; i<3 ; i++  ){
-    for(let j = 0; j <3 ; j++){
-        console.log(j, i);
-    }
+const GameBoard = document.querySelector("#gameboard");
+
+const  InfoDisplay = document.querySelector("#info");
+
+const VectorTTT = ["","", "",
+"","", "",
+"","", ""
+];
+
+function createBoard(){
+    VectorTTT.forEach((cell, index) =>{
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        console.log('Element Created');
+        GameBoard.append(cellElement)
+    });
 }
+
+createBoard()
+
+
+
+
+// for(let i = 0; i<3 ; i++  ){
+//     for(let j = 0; j <3 ; j++){
+//         console.log(j, i);
+//     }
+// }
