dea70690f096c4790ff724e1e2b906db3fecb2ff
cr7st74n <cris-082011@hotmail.com>
2023-05-16 23:40:45 -0400

    last changes to my app: adding shapes and classes to create the icons

--- JS/TicTacToe.js
+++ JS/TicTacToe.js
@@ -8,17 +8,43 @@ const VectorTTT = ["","", "",
 "","", ""
 ];
 
+
+InfoDisplay.innerHTML= "First Player"
+InfoDisplay.classList.add("player1")
+
+
 function createBoard(){
+
     VectorTTT.forEach((cell, index) =>{
         const cellElement = document.createElement('div');
         cellElement.classList.add('cell');
+        cellElement.addEventListener("click", ()=>{
+            if(InfoDisplay.className){
+                insertCircle(cellElement)
+                InfoDisplay.classList.remove("player1");
+            }else {
+                insertxElement(cellElement)
+                InfoDisplay.classList.add("player1");
+            }
+           
+        })
         console.log('Element Created');
         GameBoard.append(cellElement)
     });
+
 }
 
 createBoard()
 
+function insertCircle(element){
+    element.classList.remove("cell")
+    element.classList.add("circle")
+}
+
+function insertxElement(element){
+    element.classList.remove("cell")
+    element.classList.add("xElement")
+}
 
 
 
